1e737446845631c2ae4fcbb5bbe693e1d97c494a
AbrahamPadrino <abrahampadrino@gmail.com>
2022-02-03 11:18:00 -0500

    practicando if

---
+++ src/main/kotlin/if-else.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var soyUnPerro:Boolean = true
+    var nombre = "Titán"
+    var animal =  "Perro"
+    var raza = "Pitbull"
+
+    if (animal == "Perro" && raza == "Pitbull") {
+        println("Él es $nombre. Un $animal de raza $raza")
+    }else {
+        println("No soy un perro")
+    }
+}
